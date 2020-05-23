ad26f03d64a7c54b30cd7cf8dfd21d6f8b87c6a7
jportella93 <jportella93@gmail.com>
2020-05-23 11:20:47 +0200

    feat: add eslint

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+http.listen(port, () => {
+  console.log('listening on *:' + port)
+})
