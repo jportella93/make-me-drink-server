0def705fb19ee64fac75bdedb32df76108f4f2c3
jportella93 <jportella93@gmail.com>
2020-05-23 14:07:54 +0200

    feat: change data structure and different rooms

--- index.js
+++ index.js
@@ -4,38 +4,64 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
 
-let currentUsers = 0
+const users = new Map()
 const rooms = new Map()
 
-io.on('connection', (socket) => {
-  const roomName = socket.handshake.query.roomName
-  if (!roomName) return
+const getNewUser = (socket, userName) => ({
+  id: socket.id,
+  name: userName
+})
 
-  currentUsers++
-  console.log('---->: currentUsers', currentUsers)
+const getNewRoom = (roomName) => ({
+  name: roomName,
+  users: new Set()
+})
 
-  socket.join(roomName)
-  console.log(`connected to ${roomName}`)
-  if (!rooms.has(roomName)) rooms.set(roomName, {})
-  const currentRoom = rooms.get(roomName)
+function logUsers () {
+  console.log('Users\n', inspect(users, false, null, true))
+  console.log('Number of users online', users.size)
+}
 
-  const userId = socket.id
-  if (!currentRoom.users) currentRoom.users = new Set()
-  currentRoom.users.add(userId)
-
+function logRooms () {
   console.log('rooms\n', inspect(rooms, false, null, true))
   console.log('number of rooms', rooms.size)
+}
+
+const getRoomState = (roomName) => ({
+  users: [...rooms.get(roomName).users].map(id => users.get(id))
+})
+
+io.on('connection', (socket) => {
+  const { roomName, userName } = socket.handshake.query
+  if (!roomName || !userName) return
+
+  const user = getNewUser(socket, userName)
+  console.log(`${user.name} connected`)
+  users.set(user.id, user)
+  logUsers(user)
+
+  if (!rooms.has(roomName)) rooms.set(roomName, getNewRoom(roomName))
+  const room = rooms.get(roomName)
+  room.users.add(user.id)
+  socket.join(room.name)
+  console.log(`${user.name} connected to room ${room.name}`)
+  logRooms()
 
   socket.on('connection confirmation', () => {
     console.log('connection confirmation')
-    socket.emit('connection confirmation', { roomName })
+    socket.emit('connection confirmation', {
+      roomName: room.name,
+      userName: user.name
+    })
+
+    io.in(room.name).emit('room state', getRoomState(room.name))
   })
 
   socket.on('disconnecting', (reason) => {
     Object.keys(socket.rooms).forEach(roomName => {
       if (!rooms.has(roomName)) return
 
-      rooms.get(roomName).users.delete(userId)
+      rooms.get(roomName).users.delete(user.id)
       if (rooms.get(roomName).users.size === 0) {
         rooms.delete(roomName)
       }
@@ -43,11 +69,10 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', (msg) => {
-    currentUsers--
-    console.log('disconnected!')
-    console.log('---->: currentUsers', currentUsers)
-    console.log('rooms\n', inspect(rooms, false, null, true))
-    console.log('number of rooms', rooms.size)
+    console.log(`${user.name} disconnected`)
+    users.delete(user.id)
+    logUsers()
+    logRooms()
   })
 })
 
