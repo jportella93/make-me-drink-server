cb24f72cbba97074e6b55675a783f86c4055fb8d
jportella93 <jportella93@gmail.com>
2020-05-23 13:25:02 +0200

    feat: basic ws rooms

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 const app = require('express')()
+const inspect = require('util').inspect
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
 
 let currentUsers = 0
+const rooms = new Map()
+
 io.on('connection', (socket) => {
+  const roomName = socket.handshake.query.roomName
+  if (!roomName) return
+
   currentUsers++
-  console.log('connected!')
   console.log('---->: currentUsers', currentUsers)
+
+  socket.join(roomName)
+  console.log(`connected to ${roomName}`)
+  if (!rooms.has(roomName)) rooms.set(roomName, {})
+  const currentRoom = rooms.get(roomName)
+
+  const userId = socket.id
+  if (!currentRoom.users) currentRoom.users = new Set()
+  currentRoom.users.add(userId)
+
+  console.log('rooms\n', inspect(rooms, false, null, true))
+  console.log('number of rooms', rooms.size)
 
   socket.on('connection confirmation', () => {
     console.log('connection confirmation')
-    socket.emit('connection confirmation')
+    socket.emit('connection confirmation', { roomName })
+  })
+
+  socket.on('disconnecting', (reason) => {
+    Object.keys(socket.rooms).forEach(roomName => {
+      if (!rooms.has(roomName)) return
+
+      rooms.get(roomName).users.delete(userId)
+      if (rooms.get(roomName).users.size === 0) {
+        rooms.delete(roomName)
+      }
+    })
   })
 
   socket.on('disconnect', (msg) => {
-    console.log('disconnected!')
     currentUsers--
+    console.log('disconnected!')
     console.log('---->: currentUsers', currentUsers)
+    console.log('rooms\n', inspect(rooms, false, null, true))
+    console.log('number of rooms', rooms.size)
   })
 })
 
